edc15e71f5e3d47b8e87a23ec73944c92cfda2b7
pratouis <6393062+pratouis@users.noreply.github.com>
2024-10-23 14:32:17 -0500

    import and play with lichen

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,18 @@
-function setup() {
+let lichen;
 
+function preload() {
+    lichen = loadImage("assets/lichen_pegs.jpeg");
+};
+
+function setup() {
+    // size doesn't work
+    createCanvas(600,400);
+    // imageMode(CENTER);
 };
 
 function draw() {
-
+    background(244);
+    tint(255,mouseY%255,mouseX%255);
+    image(lichen,0,0,mouseX,mouseY);
 };
 
