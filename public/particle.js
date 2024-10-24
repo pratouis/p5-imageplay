5a453ba56fb6ce337e266a773878ec29b11f166c
pratouis <6393062+pratouis@users.noreply.github.com>
2024-10-24 18:20:22 -0500

    remove copies

---
+++ public/particle.js
@@ -0,0 +1,5 @@
+// Particle class would go here
+class Particle {
+    constructor() {
+    }
+  }
--- public/sketch.js
+++ public/sketch.js
@@ -1,157 +1,66 @@
-let frog, preImg;
-
+let frog;
+let pointilize = 25;
 function preload() {
-    frog = loadImage('./assets/small_frog.jpg');
-    // frog = loadImage('./assets/frog.jpg')
+    // frog = loadImage('./assets/small_frog.jpg');
+    frog = loadImage('./assets/frog.jpg')
 }
 
 function setup() {
-    preImg = createImage(frog.width, frog.height);
-    // createCanvas(frog.width,frog.height);
-    createCanvas(preImg.width, preImg.height);
+    createCanvas(frog.width, frog.height);
     pixelDensity(1);
-    preProcessImage();
+    background(0);
     // noLoop();
 };
 
 function draw() {
-    // image(frog, 0, 0);
-    // myImageMethod();
-    // centerShine();
-    // bwFilter();
-    postProcessImage();
-    image(preImg, 0,0);
+    paintPixels();
 };
 
-let first = true;
-let times = 0;
 
-function preProcessImage() {
-    preImg.loadPixels();
-    frog.loadPixels();
-    for(let i =0; i<preImg.pixels.length; i+=4) {
-        let [r,g,b,alpha] = frog.pixels.slice(i, i+4);
-        // let x = i/4 % width;
-        // let alpha = map(x, 0, preImg.width, 0, 255);
-        preImg.pixels[i] = r;
-        preImg.pixels[i+1] = g;
-        preImg.pixels[i+2] = b;
-        preImg.pixels[i+3] = alpha;
-    }
-    preImg.updatePixels();
-    image(preImg,0,0);
-
-}
-
-function myImageMethod() {
-        loadPixels();
-        frog.loadPixels();
+function spotPaint() {
+    for(let i =0; i< 100; i++) {
+        let x = random(width);
+        let y = random(height);
         
-        for(let i = 0; i<pixels.length; i+=4) {
-            let x = i/4 % width;
-            let y = Math.floor(i/4/width);
-            // center
-            let d = dist(width/2, height/2, x,y);
-            // flashlight
-            // let d = dist(mouseX, mouseY, x, y);
-            let factor = map(d, 0, 1000, 2, 0);
-            let frogPixel = frog.pixels.slice(i, i+4);
-            let pColor = color(...frogPixel);
-            let brighty = brightness(pColor);
-            let [r,g,b,a] = frogPixel;
-            pixels[i] = r*factor;
-            pixels[i+1] = g*factor;
-            pixels[i+2] = b*factor;
-            pixels[i+3] = a;
-        }
-        updatePixels();
-}
-
-function centerShine() {
-    loadPixels();
-    frog.loadPixels();
-    
-    for(let i = 0; i<pixels.length; i+=4) {
-        let x = i/4 % width;
-        let y = Math.floor(i/4/width);
-        // center
-        let d = dist(width/2, height/2, x,y);
-        // TODO look at map in p5
-        let factor = map(d, 0, 1000, 2, 0)
-        let [r,g,b,a] = frog.pixels.slice(i, i+4);
-        pixels[i] = r*factor;
-        pixels[i+1] = g*factor;
-        pixels[i+2] = b*factor;
-        pixels[i+3] = a;
-    };
-    updatePixels();
+        let c = frog.get(x,y);
+        c[3] = c[3]/2;
+        fill(c);
+        noStroke();
+        ellipse(x,y,15,15);
+    }
 }
 
 /**
- * slowish
+ * exercise 1
+ * paint using pixels
  */
-function flashlight() {
-    loadPixels();
+
+function paintPixels() {
+    // console.log('being called?');
     frog.loadPixels();
-    
-    for(let i = 0; i<pixels.length; i+=4) {
-        let x = i/4 % width;
-        let y = Math.floor(i/4/width);
-
-        // flashlight
-        let d = dist(mouseX, mouseY, x, y);
-        let factor = map(d, 0, 1000, 2, 0);
-        let [r,g,b,a] = frog.pixels.slice(i, i+4);
-        pixels[i] = r*factor;
-        pixels[i+1] = g*factor;
-        pixels[i+2] = b*factor;
-        pixels[i+3] = a;
-    };
-    updatePixels();
-}
-
-function bwFilter() {
-    loadPixels();
-    frog.loadPixels();
-
-        
-    for(let i = 0; i<pixels.length; i+=4) {
-        let colors = frog.pixels.slice(i, i+4);
-        let pColor = color(...colors);
-        let brightVal = brightness(pColor);
-        if (brightVal > mouseX%100) {
-            pixels[i] = 255;
-            pixels[i+1] = 255;
-            pixels[i+2] = 255;
-        } else {
-            pixels[i] = 0;
-            pixels[i+1] = 0;
-            pixels[i+2] = 0;
-        }
-        pixels[i+3] = 255;
-    };
-    updatePixels();
-}
-
-function postProcessImage() {
-    if (frameCount % 90 === 0) {
-        preImg.loadPixels();
-        for(let i =0; i<preImg.pixels.length; i+=4) {
-            let [r,g,b,_] = frog.pixels.slice(i, i+4);
-            let x = i/4 % width;
-            let alpha = map(x, 0, preImg.width, 0, 255);
-            let red = r;
-            let green = g;
-            if ((frameCount/90) % 2 == 0) {
-                red = map(r, 0, 255, 0, 100);
-            } else {
-                green = map(g,0, 255, 0, 100);
-            }
-            preImg.pixels[i] = red;
-            preImg.pixels[i+1] = green;
-            preImg.pixels[i+2] = b;
-            preImg.pixels[i+3] = alpha;
-        }
-        preImg.updatePixels();
+    let times = 0;
+    while(times++ < 50) {
+        let x = floor(random(width));
+        let y = floor(random(height));
+        let i = 4* (x + y*width);
+        let colorArr = frog.pixels.slice(i, i+4);
+        colorArr[3] = 200;
+        // if (frameCount % 30 === 0 && times % 100 === 0) {
+        //     console.log('colorArr :: ', colorArr);
+        // }
+        noStroke();
+        fill(...colorArr);
+        ellipse(x,y,pointilize,pointilize);
     }
 }
+
+/**
+ * exercise no 2
+ * particles
+ * draw image as particles where they take particles with them
+ */
+
+/**
+ * exercise no 3
+ * 2D -> 3D 
+ */
