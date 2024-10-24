650f70a16191a127940bfe20ca43b584731ae2c2
pratouis <6393062+pratouis@users.noreply.github.com>
2024-10-24 17:20:05 -0500

    prepare for postProcessMoving

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,6 @@
-let photo, preImg;
+let photo, preImg, nextImg;
+
+const EDGE_THRESHOLD = 2;
 function preload() {
     photo = loadImage('./assets/photo.jpg');
 }
@@ -9,38 +11,27 @@ function setup() {
     createCanvas(photo.width,photo.height);
     pixelDensity(1);
     preImg = createImage(photo.width, photo.height);
+    nextImg = createImage(photo.width,photo.height);
     preProcessImage();
 };
 
 function draw() {
-    // image(preImg,0,0);
-    postProcessImage();
+    image(preImg,0,0);
+    // postProcessImage();
 };
+
 function preProcessImage() {
     preImg.loadPixels();
     photo.loadPixels();
+    // make edges pop
     for(let i =0; i<preImg.pixels.length; i+=4) {
-        let [r,g,b,alpha] = photo.pixels.slice(i, i+4);
-        preImg.pixels[i] = r;
-        preImg.pixels[i+1] = g;
-        preImg.pixels[i+2] = b;
-        preImg.pixels[i+3] = alpha;
-    }
-    preImg.updatePixels();
-    image(preImg,0,0);
-}
-
-function postProcessImage() {
-    preImg.loadPixels();
-    for(let i =0; i<preImg.pixels.length; i+=4) {
-        let color1 = preImg.pixels.slice(i, i+4);
-        let [r,g,b,a] = color1;
+        let color1 = photo.pixels.slice(i, i+4);
         let b1 = brightness(color(...color1));
         let i2 = getNeighborStart(i);
-        let color2 = preImg.pixels.slice(i2, i2+4);
+        let color2 = photo.pixels.slice(i2, i2+4);
         let b2 = brightness(color(...color2));
         let diff = abs(b2-b1);
-        if (diff > 2) {
+        if (diff > EDGE_THRESHOLD) {
             preImg.pixels[i] = 0;
             preImg.pixels[i+1] = 0;
             preImg.pixels[i+2] = 0;
@@ -53,7 +44,6 @@ function postProcessImage() {
     }
     preImg.updatePixels();
     image(preImg,0,0);
-
 }
 
 function getNeighborStart(i) {
@@ -67,4 +57,15 @@ function getNeighborStart(i) {
         neighbor = i-preImg.width;
     }
     return neighbor
+}
+
+
+function postProcessImage() {
+    if (frameCount % 30 === 0) {
+        if (frameCount/30 % 2 === 0) {
+            // move leftish
+        } else {
+            // move rightish
+        }
+    }
 }
